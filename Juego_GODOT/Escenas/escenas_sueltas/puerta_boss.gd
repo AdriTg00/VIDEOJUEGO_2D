568d6def49e40e74ad4d88b6fab7257c9c47df4a
AdriTg00 <adrian2000tg@gmail.com>
2025-12-02 14:22:03 +0100

    Update escenario3.tscn tile map data
    Modified the tile_map_data property in the Foreground TileMapLayer of escenario3.tscn. This likely reflects changes to the level layout or tile arrangement in the scene.

--- Juego_GODOT/Escenas/escenas_sueltas/puerta_boss.gd
+++ Juego_GODOT/Escenas/escenas_sueltas/puerta_boss.gd
@@ -1,14 +1,10 @@
 extends Node2D
 
-@export var distancia_bajada := 96.0    # ajusta el valor según tu puerta
-@export var tiempo_bajada := 0.5
+@export var distancia_bajada := 70.0    # ajusta el valor según tu puerta
+@export var tiempo_bajada := 1.5
+
 
 func bajar():
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "position:y", position.y + distancia_bajada, tiempo_bajada)
 	
-	# opcional: desactivar colisión al terminar
-	if has_node("CollisionShape2D"):
-		tween.finished.connect(func():
-			$CollisionShape2D.disabled = true
-		)
--- Juego_GODOT/Escenas/trigger_puerta.gd
+++ Juego_GODOT/Escenas/trigger_puerta.gd
@@ -1,6 +1,11 @@
 extends Area2D
 @onready var puerta = get_node("../puerta_boss")
+var activado := false
 
 func _on_body_entered(body):
+	if activado:
+		return
+		
 	if body.name == "Rey":
+		activado = true
 		puerta.bajar()
